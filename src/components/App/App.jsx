48db6440d9d04bca359571d47205b5f1f6d8ccb7
PoliukhovychAnna <116736506+PoliukhovychAnna@users.noreply.github.com>
2023-08-02 04:02:32 +0300

    completed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,6 +6,8 @@ import {
   PhonebookContainer,
   WrapperTask,
 } from './Styled.App';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { ContactList } from '../ContactList/ContactList';
 import { ContactForm } from '../ContactForm/ContactForm';
 import { Filter } from '../Filter/Filter';
@@ -15,20 +17,14 @@ const myContacts = [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },]
 
+const savedContacts = localStorage.getItem('contacts');
+
 export const App = () => {
 
-  const [contacts, setContacts] = useState(myContacts)
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(savedContacts) ?? myContacts
+  });
   const [filter, setFilter] = useState('')
-
-  useEffect(() => {
-      const savedContacts = localStorage.getItem('contacts');
-
-    if (savedContacts !== null) {
-      const parsedContacts = JSON.parse(savedContacts);
-      setContacts(parsedContacts);
-      return;
-    }
-  }, [])
 
   useEffect(() => {
    localStorage.setItem('contacts', JSON.stringify(contacts));
@@ -40,17 +36,24 @@ export const App = () => {
     contacts.find(contact =>
       contact.name.toLowerCase().includes(addedContact.name.toLowerCase())
     )
-      ? alert(`${addedContact.name} is already in contacts`)
-      : setContacts(prevState => (
-        [
-            {
-              id: addedContact.id,
-              name: addedContact.name,
-              number: addedContact.number,
-            },
-            ...prevState,
-          ]
-      ))
+      ? toast.warn(`${addedContact.name} is already in contacts`, {
+          position: 'top-right',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'light',
+        })
+      : setContacts(prevState => [
+          {
+            id: addedContact.id,
+            name: addedContact.name,
+            number: addedContact.number,
+          },
+          ...prevState,
+        ]);
   };
 
   const deleteContact = contactId => {
@@ -77,102 +80,10 @@ export const App = () => {
         <ContactContainer>
           <ContactsTitle>Contacts</ContactsTitle>
           <Filter onChange={filterValue} value={filter} />
-          <ContactList
-            contacts={contactFilter()}
-            onDelete={deleteContact}
-          />
+          <ContactList contacts={contactFilter()} onDelete={deleteContact} />
         </ContactContainer>
+        <ToastContainer />
       </WrapperTask>
     );
   }
 
-
-
-
-// export class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-//     filter: '',
-//   };
-
-//   componentDidMount() {
-//     const savedContacts = localStorage.getItem('contacts');
-
-//     if (savedContacts !== null) {
-//       const parsedContacts = JSON.parse(savedContacts);
-//       this.setState({ contacts: parsedContacts });
-//       return;
-//     }
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     if (this.state.contacts !== prevState.contacts) {
-//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//     }
-//   }
-
-//   addContact = addedContact => {
-//     const { contacts } = this.state;
-//     contacts.find(contact =>
-//       contact.name.toLowerCase().includes(addedContact.name.toLowerCase())
-//     )
-//       ? alert(`${addedContact.name} is already in contacts`)
-//       : this.setState(prevState => ({
-//           contacts: [
-//             {
-//               id: addedContact.id,
-//               name: addedContact.name,
-//               number: addedContact.number,
-//             },
-//             ...prevState.contacts,
-//           ],
-//         }));
-//   };
-
-//   deleteContact = contactId => {
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-//     }));
-//   };
-
-//   filterValue = e => {
-//     this.setState({
-//       filter: e.target.value.toLowerCase(),
-//     });
-//   };
-
-//   contactFilter = () => {
-//     const { filter, contacts } = this.state;
-//     const normalizedFilter = filter.toLowerCase();
-//     console.log(contacts);
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(normalizedFilter)
-//     );
-//   };
-
-//   render() {
-//     const filteredSearch = this.contactFilter();
-//     console.log(this.state.contacts);
-//     return (
-//       <WrapperTask>
-//         <PhonebookContainer>
-//           <PhonebookTitle>Phonebook</PhonebookTitle>
-//           <ContactForm onSubmit={this.addContact} />
-//         </PhonebookContainer>
-//         <ContactContainer>
-//           <ContactsTitle>Contacts</ContactsTitle>
-//           <Filter onChange={this.filterValue} value={this.state.filter} />
-//           <ContactList
-//             contacts={filteredSearch}
-//             onDelete={this.deleteContact}
-//           />
-//         </ContactContainer>
-//       </WrapperTask>
-//     );
-//   }
-// }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -9,6 +9,8 @@ import { nanoid } from 'nanoid';
 export const ContactForm = ({onSubmit}) => {
   const [name, setName] = useState('')
   const [number, setNumber] = useState('')
+  console.log(name);
+  console.log(number);
 
  const handleSubmit = e => {
     e.preventDefault();
@@ -62,64 +64,6 @@ export const ContactForm = ({onSubmit}) => {
       </ContactFormContainer>
     );
   }
-
-
-// export class ContactForm  extends Component{
-//   state = {
-//     name: '',
-//     number: '',
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     const newContact = {
-//       name: this.state.name,
-//       number: this.state.number,
-//       id: nanoid(),
-//     };
-
-//     this.props.onSubmit(newContact);
-//     this.setState({ name: '', number: '' });
-//   };
-
-//   handleChange = e => {
-//     this.setState({
-//       [e.target.name]: e.target.value,
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <ContactFormContainer onSubmit={this.handleSubmit}>
-//         <label>
-//           <h4>Name</h4>
-//           <input
-//             type="text"
-//             name="name"
-//             value={this.state.name}
-//             onChange={this.handleChange}
-//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//             required
-//           />
-//         </label>
-//         <label>
-//           <h4>Number</h4>
-//           <input
-//             type="tel"
-//             name="number"
-//             value={this.state.number}
-//             onChange={this.handleChange}
-//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//             required
-//           />
-//         </label>
-//         <button type="submit">Add contact</button>
-//       </ContactFormContainer>
-//     );
-//   }
-// }
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired
